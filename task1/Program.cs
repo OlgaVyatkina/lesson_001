0f405857832f0d19615e57b4797612d48ae90221
Olga Vyatkina <calisto999@mail.ru>
2022-12-01 15:33:20 +0300

    add tasks

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+Console.WriteLine("Первое число " + a + " больше чем второе " + b);
+}
+else
+{
+Console.WriteLine("Второе число " + b + " больше чем первое " + a);
+}
--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + N + ":");
+
+for (int i = 1; i <= N; i++)
+{ 
+  if (i % 2 == 0)
+  {
+    Console.Write(i + " ");
+  }
+}
